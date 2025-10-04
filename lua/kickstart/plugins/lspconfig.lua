ab47451bf3ed3274e4dbad1fdcd3a2321fc353fd
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-10-03 18:04:28 -0700

    Comment out copilot-language-server in LSP config

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -221,7 +221,7 @@ return {
         jsonls = {},
         sqlls = {},
         rust_analyzer = {},
-        ["copilot-language-server"] = {},
+        -- ["copilot-language-server"] = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
