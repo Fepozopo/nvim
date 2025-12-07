bff0a44a2857c616792c7e5afaae9c71763924d7
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-12-07 15:30:22 -0800

    Integrate copilot-lsp and remap accept key
    Move copilot_nes_debounce init into the dependency's init and comment
    out the standalone custom.plugins.copilot-lsp entry Change
    nes.accept_and_goto mapping from <leader>y to <M-j>

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,11 +1,12 @@
 return {
   'zbirenbaum/copilot.lua',
+  dependencies = { 'copilotlsp-nvim/copilot-lsp' },
   requires = {
       "copilotlsp-nvim/copilot-lsp",
+      init = function()
+        vim.g.copilot_nes_debounce = 500
+      end,
     },
-  init = function()
-    vim.g.copilot_nes_debounce = 500
-  end,
   cmd = 'Copilot',
   event = 'InsertEnter',
   config = function()
@@ -42,7 +43,7 @@ return {
       nes = {
         enabled = true,
         keymap = {
-            accept_and_goto = "<leader>y",
+            accept_and_goto = "<M-j>",
             accept = false,
             dismiss = "<Esc>",
         },
