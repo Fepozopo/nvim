1beff26c5698dc4f658e1a10cac93ef67e2cfb99
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-12-14 12:40:17 -0800

    Disable bigfile, dashboard, explorer, picker

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -8,12 +8,12 @@ return {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-        bigfile = { enabled = true },
-        dashboard = { enabled = true },
-        explorer = { enabled = true },
+        bigfile = { enabled = false },
+        dashboard = { enabled = false },
+        explorer = { enabled = false },
         indent = { enabled = true },
         input = { enabled = true },
-        picker = { enabled = true },
+        picker = { enabled = false },
         notifier = { enabled = true },
         quickfile = { enabled = true },
         scope = { enabled = true },
