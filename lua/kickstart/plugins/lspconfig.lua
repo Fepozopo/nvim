e2f19a0fdd29532a8d6111d005cefffa45d2a8b7
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-12-15 19:57:24 -0800

    Remove copilot-language-server from LSP list

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -221,7 +221,6 @@ return {
         jsonls = {},
         sqlls = {},
         rust_analyzer = {},
-        -- ["copilot-language-server"] = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
